6e05438fa9da373f46c935ffee1195e39bd60224
PhilipFelipe <wakezinnsaw13@gmail.com>
2022-02-28 17:05:47 -0300

    children attr created

--- oo/person.py
+++ oo/person.py
@@ -1,19 +1,21 @@
 class Person:
-    def __init__(self, name=None, age=20):
+    def __init__(self, *children, name=None, age=20):
         #Atributo = parâmetro
         self.name = name
         self.age = age
+        self.children = list(children)
 
     def greet(self):
         return f'Hey {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Person('Alan')
-    print(Person.greet(p))
-    print(id(p))
-    print(p.greet())
-    print(p.name)
-    p.name = 'Felipe'
-    print(p.name)
-    print(p.age)
+    felipe = Person(name='Felipe')
+    luciano = Person(felipe, name='Luciano')
+    print(Person.greet(luciano))
+    print(id(luciano))
+    print(luciano.greet())
+    print(luciano.name)
+    print(luciano.age)
+    for child in luciano.children:
+        print(child.name)
