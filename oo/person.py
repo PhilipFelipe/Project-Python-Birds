8da31de2c13159856dca233f88302d71188a28ad
PhilipFelipe <wakezinnsaw13@gmail.com>
2022-02-28 16:59:38 -0300

    instance attr name and age created

--- oo/person.py
+++ oo/person.py
@@ -1,10 +1,19 @@
 class Person:
+    def __init__(self, name=None, age=20):
+        #Atributo = parâmetro
+        self.name = name
+        self.age = age
+
     def greet(self):
         return f'Hey {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Person()
+    p = Person('Alan')
     print(Person.greet(p))
     print(id(p))
     print(p.greet())
+    print(p.name)
+    p.name = 'Felipe'
+    print(p.name)
+    print(p.age)
